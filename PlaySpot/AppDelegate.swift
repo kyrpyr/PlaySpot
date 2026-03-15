c42cd2b728abfe41dc6b5e28708fd65e515e4195
Alexander Retunskiy <kyrpyr@gmail.com>
2026-03-15 12:09:59 +0700

    feat: create Xcode project skeleton

---
+++ PlaySpot/AppDelegate.swift
@@ -0,0 +1,11 @@
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Setup will go here
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Teardown will go here
+    }
+}
---
+++ PlaySpotTests/PlaySpotTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import PlaySpot
+
+final class PlaySpotTests: XCTestCase {
+    func testExample() throws {
+        // Placeholder test
+        XCTAssertTrue(true)
+    }
+}
