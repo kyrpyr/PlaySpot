6927fe12f6d2e95d512f5f2eb138ba02ad44f9c8
Alexander Retunskiy <kyrpyr@gmail.com>
2026-03-17 13:42:46 +0700

    Hoist GitHub URL to static let and normalize toggle indentation

--- PlaySpot/ContentView.swift
+++ PlaySpot/ContentView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    private static let githubURL = URL(string: "https://github.com/kyrpyr/PlaySpot")!
+
     @EnvironmentObject var state: AppState
     var body: some View {
         VStack(spacing: 20) {
@@ -24,12 +26,12 @@ struct ContentView: View {
                     get: { state.launchAtLogin },
                     set: { state.launchAtLogin = $0 }
                 ))
-                .toggleStyle(.checkbox)
-                .focusable(false)
+                    .toggleStyle(.checkbox)
+                    .focusable(false)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
 
-            Link("Source Code on GitHub ↗", destination: URL(string: "https://github.com/kyrpyr/PlaySpot")!)
+            Link("Source Code on GitHub ↗", destination: Self.githubURL)
                 .foregroundStyle(.tertiary)
                 .font(.system(size: 11))
                 .focusable(false)
